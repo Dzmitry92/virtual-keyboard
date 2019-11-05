3ffa58983f2efabd354b38d6cc62a001d320bf6c
Dmitry <teramisu01@gmail.com>
2019-11-06 00:32:17 +0300

    feat: add buttons

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ const keyboard = [
                 ru: 'ё',
             }
         },
-        {
-            '48':   {
-                en: '0',
-                ru: '0',
-            }
-        },
 
         {
             '49':   {
@@ -28,12 +22,264 @@ const keyboard = [
         },
 
         {
+            '51':   {
+                en: '3',
+                ru: '3',
+            }
+        },
+
+        {
+            '52':   {
+                en: '4',
+                ru: '4',
+            }
+        },
+
+        {
+            '53':   {
+                en: '5',
+                ru: '5',
+            }
+        },
+
+        {
+            '54':   {
+                en: '6',
+                ru: '6',
+            }
+        },
+
+        {
+            '55':   {
+                en: '7',
+                ru: '7',
+            }
+        },
+
+        {
+            '56':   {
+                en: '8',
+                ru: '8',
+            }
+        },
+
+        {
+            '57':   {
+                en: '9',
+                ru: '9',
+            }
+        },
+
+        {
+            '48':   {
+                en: '0',
+                ru: '0',
+            }
+        },
+
+        {
+            '189':   {
+                en: '-',
+                ru: '-',
+            }
+        },
+
+        {
+            '187':   {
+                en: '=',
+                ru: '=',
+            }
+        },
+
+        {
             '8':   {
                 en: 'backspace',
                 ru: 'backspace',
             }
         },        
     ],
+
+    [
+        {
+            '9':   {
+                en: 'tab',
+                ru: 'tab',
+            }
+        },
+
+        {
+            '81':   {
+                en: 'q',
+                ru: 'й',
+            }
+        },
+
+        {
+            '87':   {
+                en: 'w',
+                ru: 'ц',
+            }
+        },
+
+        {
+            '69':   {
+                en: 'e',
+                ru: 'у',
+            }
+        },
+
+        {
+            '82':   {
+                en: 'r',
+                ru: 'к',
+            }
+        },
+
+        {
+            '84':   {
+                en: 't',
+                ru: 'е',
+            }
+        },
+
+        {
+            '89':   {
+                en: 'y',
+                ru: 'н',
+            }
+        },
+
+        {
+            '85':   {
+                en: 'u',
+                ru: 'г',
+            }
+        },
+
+        {
+            '73':   {
+                en: 'i',
+                ru: 'ш',
+            }
+        },
+
+        {
+            '79':   {
+                en: 'o',
+                ru: 'щ',
+            }
+        },
+
+        {
+            '80':   {
+                en: 'p',
+                ru: 'з',
+            }
+        },
+
+        {
+            '219':   {
+                en: '[',
+                ru: 'х',
+            }
+        },
+
+        {
+            '221':   {
+                en: ']',
+                ru: 'ъ',
+            }
+        },
+
+        {
+            '13':   {
+                other: 'Enter',
+                en: ' \n',
+                ru: ' \n',
+            }
+        },
+    ],
+
+    [
+        {
+        '20':   {
+            en: 'CapsLock',
+            ru: 'CapsLock',
+            }
+        },
+        {
+            '65':   {
+                en: 'a',
+                ru: 'ф',
+            }
+        },
+        {
+            '83':   {
+                en: 's',
+                ru: 'ы',
+            }
+        },
+        {
+            '68':   {
+                en: 'd',
+                ru: 'в',
+            }
+        },
+        {
+            '70':   {
+                en: 'f',
+                ru: 'а',
+            }
+        },
+        {
+            '71':   {
+                en: 'g',
+                ru: 'п',
+            }
+        },
+        {
+            '72':   {
+                en: 'h',
+                ru: 'р',
+            }
+        },
+        {
+            '74':   {
+                en: 'j',
+                ru: 'о',
+            }
+        },
+        {
+            '75':   {
+                en: 'k',
+                ru: 'л',
+            }
+        },
+        {
+            '76':   {
+                en: 'l',
+                ru: 'д',
+            }
+        },
+        {
+            '186':   {
+                en: ';',
+                ru: 'ж',
+            }
+        },
+        {
+            '222':   {
+                en: '"',
+                ru: 'э',
+            }
+        },
+        {
+            '220':   {
+                en: "",
+                ru: '',
+            }
+        },
+    ]
 ]
 
 let currentLang = 'en';
@@ -82,6 +328,9 @@ document.addEventListener('keyup', function(e) {
    
 
 keyboard.forEach(function(row) {
+    const rowWraper= document.createElement('div');
+
+
     row.forEach(function(buttonObj) {
         const key = Object.keys(buttonObj)[0];
         const button = document.createElement('button');
@@ -89,6 +338,8 @@ keyboard.forEach(function(row) {
         button.setAttribute('fff', buttonObj[key][currentLang]);
         button.classList.add('button');
         button.innerHTML = buttonObj[key][currentLang];
-        root.appendChild(button);
+        rowWraper.appendChild(button);
     });
-})
+    root.appendChild(rowWraper);
+
+});
